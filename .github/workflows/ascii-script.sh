80c598c0275f0ed4712e199972e002f658b67148
chris-kyle2 <22ms0010@iitism.ac.in>
2025-03-26 23:15:47 +0530

    Learning Workflows

---
+++ .github/workflows/ascii-script.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+sudo apt-get update -y
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run For Your Life,I am  DROGON RAWWWR" >> dragon.txt
+ls -la
+cat dragon.txt
+
+
---
+++ .github/workflows/ascii-script1.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+sudo apt-get update -y
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run For Your Life,I am  DROGON RAWWWR and I am the second biggest dragon in the world" >> dragon.txt
+ls -la
+cat dragon.txt
+
+
